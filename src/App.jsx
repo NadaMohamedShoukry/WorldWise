f1ab8f702fcbd4bc4a7d25b4c08d8e676bfec4ba
NadaMohamedShoukry <nadashoukry23@gmail.com>
2025-08-28 19:13:32 +0300

    used React Router

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout/AppLayout";
+import Login from "./pages/Login/Login";
+import CityList from "./components/CityList/CityList";
+import { useEffect, useState } from "react";
+import CountryList from "./components/CountryList/CountryList";
+import City from "./components/City/City";
+import Form from "./components/Form/Form";
+const BASE_URL = "http://localhost:5000";
+function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+  return (
+    //if i want a piece of code to show in all pages write it here.
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities" />} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
+        </Route>
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/CountryList/CountryList.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+import CountryItem from "../CountryItem/CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "../Spinner/Spinner";
+import Message from "../Message/Message";
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message={"Add your first city by clicking on the map"} />;
+  //Apply the reduce method to collect all the unique countries in one array
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
